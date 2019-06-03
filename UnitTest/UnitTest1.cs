ae622a02d0a35affc067aad03c58b71502281c0b
VoimiX <sitnikov.alpha@mail.ru>
2019-06-04 00:46:49 +0300

    Data Test

--- UnitTest/UnitTest1.cs
+++ UnitTest/UnitTest1.cs
@@ -1,7 +1,8 @@
 ﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+
 using FigureLib;
 using FigureLib.Figures;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace UnitTest
 {
@@ -18,19 +19,35 @@ namespace UnitTest
             Assert.IsTrue(Math.Abs(c.Square - 78.539) < error);
         }
 
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void Test_Circle_InvalidRadius_ThrowsException()
+        {
+            var c = FigureFabric.CreateCircle(-5);
+        }
 
         [TestMethod]
-        public void Test_TriangeSquare_IsCorrect()
+        public void Test_TriangleSquare_IsCorrect()
         {         
             var c = FigureFabric.CreateTriange(2, 3, 3.605);
             Assert.IsTrue(Math.Abs(c.Square - 3) < error);
         }
 
         [TestMethod]
-        public void Test_Triange_IsRectangular()
+        public void Test_Triangle_IsRectangular()
         {           
             var t = FigureFabric.CreateTriange(2, 3, 3.605);
             Assert.IsTrue(((Triangle)t).IsRectangular);
+        }
+
+        [DataTestMethod]
+        [DataRow(2, 1, -1)]
+        [DataRow(-2, 1, 4)]
+        [DataRow(0, -4, 1)]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void Test_Triangle_InvalidSide_ThrowsException(double a, double b, double c)
+        {
+            FigureFabric.CreateTriange(a, b, c);
         }
     }
 }
